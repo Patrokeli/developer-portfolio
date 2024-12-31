9991545fc426b89ac70c85d188919fd23000582a
Patrokeli <patrokelimrem@gmail.com>
2024-12-31 04:40:24 -0800

    added profile pic

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,8 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
-          </p>
+            © Developer Portfolio by Patrokeli</p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "PATROKELI MREMI",
-  profile: "/profile.png",
+  profile: "/profile.jpeg",
   designation: "Software Developer",
   description:
     "My name is PATROKELI MREMI. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and aim to make the web more open to the world. My core skills are based on JavaScript and Laravel, and I enjoy doing most of my work using these technologies. I am available for any kind of job opportunity that suits my skills and interests",
